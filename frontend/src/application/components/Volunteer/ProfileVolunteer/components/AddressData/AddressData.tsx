ad466bd221df987eae42393b4514d6177dc09d05
Samuel <oiranca@gmail.com>
2023-10-18 19:13:17 +0100

    created AddressData component

---
+++ frontend/src/application/components/Volunteer/ProfileVolunteer/components/AddressData/AddressData.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { FieldForm } from '../../../../../../infrastructure/view/components/molecules/FieldForm';
+import { FormSelect } from '../../../../../../infrastructure/view/components/molecules/FormSelect';
+import { useCorrectFormat } from '../../../../../../infrastructure/hooks/useCorrectFormat';
+import './AddressData.scss';
+
+export const AddressData: React.FC<{}> = () => {
+  const { check, data, town, messageInfoUser, setInputValue, setNameEvent } =
+    useCorrectFormat();
+
+  return (
+    <form className={'AddressData'}>
+      <header className={'addressDataTitle'}>
+        <h2>Localización</h2>
+      </header>
+      <section className={'localization'}>
+        <FieldForm
+          title="Provincia"
+          name="province"
+          type="text"
+          value={data.province}
+          disabled={true}
+        />
+        <FieldForm
+          title="Isla"
+          name="island"
+          type="text"
+          stateValidate={check.island}
+          disabled={true}
+          value={data.island}
+        />
+        <FieldForm
+          title="Código Postal"
+          name="zipcode"
+          type="text"
+          stateValidate={check.zipCode}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+            setNameEvent(e.target.name);
+          }}
+          messageInfoUser={messageInfoUser.zipCode}
+        />
+      </section>
+      <section className={'address'}>
+        <FieldForm
+          title="Dirección"
+          name="address"
+          type="text"
+          stateValidate={check.address}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+            setNameEvent(e.target.name);
+          }}
+          messageInfoUser={messageInfoUser.address}
+        />
+
+        <FormSelect
+          name={'town'}
+          optionsList={town}
+          text={'Ciudad'}
+          onChange={(e) => {
+            setInputValue(e.target.value);
+            setNameEvent(e.target.name);
+          }}
+        />
+      </section>
+    </form>
+  );
+};
