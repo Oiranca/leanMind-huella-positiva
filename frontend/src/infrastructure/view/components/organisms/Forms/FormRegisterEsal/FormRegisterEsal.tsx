4d88c736c7c31c308e6f5b0013bc77d70d147bf1
Samuel <oiranca@gmail.com>
2023-11-05 11:50:56 +0000

    created Assent component from FormRegisterEsal and fixed padding

--- frontend/src/infrastructure/view/components/organisms/Forms/FormRegisterEsal/FormRegisterEsal.tsx
+++ frontend/src/infrastructure/view/components/organisms/Forms/FormRegisterEsal/FormRegisterEsal.tsx
@@ -2,10 +2,8 @@ import * as React from 'react';
 import './FormRegisterEsal.scss';
 import { ContactPersonRegister } from './component/ContactPersonRegister/ContactPersonRegister';
 import { OrganizationRegister } from './component/OrganizationRegister/OrganizationRegister';
-import { Label } from '../../../atoms/Label';
-import { FormRadio } from '../../../molecules/FormRadio';
-import { LinkText } from '../../../atoms/LinkText';
 import { SubmitButton } from '../../../atoms/SubmitButton';
+import { Assent } from './component/Assent/Assent';
 
 export const FormRegisterEsal: React.FC<{}> = () => {
   const handleSubmit = async (event: React.FormEvent) => {
@@ -16,38 +14,8 @@ export const FormRegisterEsal: React.FC<{}> = () => {
     <form className="FormRegisterEsal" method={'POST'} id="form" onSubmit={handleSubmit}>
       <ContactPersonRegister />
       <OrganizationRegister />
-      <section>
-        <Label text={'Consentimientos *'} />
-        <section className={'data-protection'}>
-          <FormRadio
-            title={'Política Privacidad'}
-            type={'checkbox'}
-            name={'Política Privacidad'}
-            value={''}
-            checked={false}
-          />
-          <p>
-            Estoy de acuerdo de con la {''}
-            <LinkText to={'/'} text={'Política de privacidad'} />.
-          </p>
-        </section>
-        <section className={'data-protection'}>
-          <FormRadio
-            title={'Protección de datos'}
-            type={'checkbox'}
-            name={'Proteccion de datos'}
-            value={''}
-            checked={false}
-          />
-          <p>
-            Estoy de acuerdo de con la {''}
-            <LinkText to={'/'} text={'Política de protección de datos'} />.
-          </p>
-        </section>
-        <div className={'button-register'}>
-          <SubmitButton text={'Registrar Entidad'} />
-        </div>
-      </section>
+      <Assent />
+      <SubmitButton text={'Registrar Entidad'} />
     </form>
   );
 };
---
+++ frontend/src/infrastructure/view/components/organisms/Forms/FormRegisterEsal/component/Assent/Assent.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { FormRadio } from '../../../../../molecules/FormRadio';
+import { LinkText } from '../../../../../atoms/LinkText';
+import './Assent.scss';
+
+export const Assent: React.FC<{}> = () => {
+  return (
+    <article className={'Assent'}>
+      <header>
+        <h1>Consentimientos</h1>
+      </header>
+        <section className={'isAssent'}>
+          <FormRadio
+            title={'Política Privacidad'}
+            type={'checkbox'}
+            name={'Política Privacidad'}
+            value={''}
+            checked={false}
+          />
+          <p>
+            Estoy de acuerdo de con la
+            <LinkText to={'/'} text={'Política de privacidad'} />.
+          </p>
+        </section>
+        <section className={'isAssent'}>
+          <FormRadio
+            title={'Protección de datos'}
+            type={'checkbox'}
+            name={'Proteccion de datos'}
+            value={''}
+            checked={false}
+          />
+          <p>
+            Estoy de acuerdo de con la
+            <LinkText to={'/'} text={'Política de protección de datos'} />.
+          </p>
+        </section>
+    </article>
+  );
+};
